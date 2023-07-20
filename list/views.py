b4e4491a956c2e00dd62c9212e8ca66b4e1427e4
KirillMelanich <kirillmelanich@gmail.com>
2023-07-20 18:44:05 +0300

    Some changes

--- list/views.py
+++ list/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from list.models import Tag, Task
+
+
+def index(request):
+    num_tags = Tag.objects.count()
+    num_tasks = Task.objects.count()
+
+    context = {
+        "num_tags": num_tags,
+        "num_tasks": num_tasks,
+    }
+
+    return render(request=request, template_name="list/index.html", context=context)
