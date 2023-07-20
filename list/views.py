ddb8c52eb92dded55825c0d4e08d64cc72f0db3e
KirillMelanich <kirillmelanich@gmail.com>
2023-07-20 18:56:28 +0300

    Create views for tasks

--- list/views.py
+++ list/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from list.models import Tag, Task
 
@@ -14,3 +16,29 @@ def index(request):
     }
 
     return render(request=request, template_name="list/index.html", context=context)
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "list/task.html"
+    context_object_name = "task_list"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("list:task-list")
+    template_name = "list/task_form.html"
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("list:task-list")
+    template_name = "list/task_form.html"
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("list:literary-format-list")
+    template_name = "list/task_confirm_delete.html"
