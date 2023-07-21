8965b563de96ed52a660c371581aaaa79c3e9a11
KirillMelanich <kirillmelanich@gmail.com>
2023-07-21 12:55:53 +0300

    Create TagViews

--- list/views.py
+++ list/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -42,3 +42,29 @@ class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("list:task-list")
     template_name = "list/task_confirm_delete.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "list/tag_list.html"
+    context_object_name = "tag_list"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("list:tag-list")
+    template_name = "list/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("list:tag-list")
+    template_name = "list/tag_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("list:tag-list")
+    template_name = "list/tag_confirm_delete.html"
